5ea055f80233b78fc94b0a88f0def7312702109d
Yuriy Li <0yuriyli@gmail.com>
2026-03-14 18:42:36 +0300

    feat: add db context and connection params

---
+++ Data/ApplicationContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+namespace TandemBackend.Data
+{
+
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using TandemBackend.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,6 +8,10 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ApplicationContext>(options =>
+    options.UseSqlite(connectionString));
 
 var app = builder.Build();
 
