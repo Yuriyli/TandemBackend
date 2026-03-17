c6e922dea7c76e273439a4fea3d803f9ab6871dd
Yuriy Li <0yuriyli@gmail.com>
2026-03-17 03:35:25 +0300

    feat: add article model

---
+++ Models/Article.cs
@@ -0,0 +1,26 @@
+﻿namespace TandemBackend.Models
+{
+    public class Article
+    {
+        public required int Id { get; set; }
+        public required string Title { get; set; }
+        public required string Content { get; set; }
+        public required string TitleRu { get; set; }
+        public required string ContentRu { get; set; }
+    }
+
+    public class ArticlePut
+    {
+        public required string Title { get; set; }
+        public required string Content { get; set; }
+        public required string TitleRu { get; set; }
+        public required string ContentRu { get; set; }
+    }
+
+    public class ArticleGet
+    {
+        public required int Id { get; set; }
+        public required string Title { get; set; }
+        public required string Content { get; set; }
+    }
+}
