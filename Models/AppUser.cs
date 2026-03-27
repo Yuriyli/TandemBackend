bacc5d958cdf6f61a142d4e68365044a41133794
Yuriy Li <0yuriyli@gmail.com>
2026-03-27 12:16:02 +0300

    feat: add Appuser model and migration

---
+++ Models/AppUser.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TandemBackend.Models
+{
+    public class AppUser
+    {
+        public required string Id { get; set; }
+        public required string Name { get; set; }
+        public required string Email { get; set; }
+        public required string Password { get; set; }
+    }
+
+    public class UserLogin
+    {
+        public required string Email { get; set; }
+        public required string Password { get; set; }
+    }
+
+    public class UserRegister
+    {
+        public required string Name { get; set; }
+
+        [EmailAddress(ErrorMessage = "Invalid emal format")]
+        public required string Email { get; set; }
+        public required string Password { get; set; }
+    }
+}
