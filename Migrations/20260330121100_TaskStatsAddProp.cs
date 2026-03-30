1ebf930bb9eef567115f97a920a6355b2a4755f2
Yuriy Li <0yuriyli@gmail.com>
2026-03-30 15:12:00 +0300

    fix: TaskStats models

---
+++ Migrations/20260330121100_TaskStatsAddProp.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TandemBackend.Migrations
+{
+    /// <inheritdoc />
+    public partial class TaskStatsAddProp : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsFinished",
+                table: "TaskStats",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsFinished",
+                table: "TaskStats");
+        }
+    }
+}
--- Models/TaskStat.cs
+++ Models/TaskStat.cs
@@ -5,6 +5,7 @@ namespace TandemBackend.Models
         public required int Id { get; set; }
         public required string UserId { get; set; }
         public required int TaskId { get; set; }
+        public required bool IsFinished { get; set; }
         public required TaskType TaskType { get; set; }
         public required TaskDifficulty TaskDifficulty { get; set; }
     }
@@ -12,9 +13,14 @@ namespace TandemBackend.Models
     public class TaskStatPost
     {
         public required int TaskId { get; set; }
-
         public required TaskType TaskType { get; set; }
+        public required TaskDifficulty TaskDifficulty { get; set; }
+    }
 
+    public class TaskStatGetResult
+    {
+        public required int TaskId { get; set; }
+        public required TaskType TaskType { get; set; }
         public required TaskDifficulty TaskDifficulty { get; set; }
     }
 }
