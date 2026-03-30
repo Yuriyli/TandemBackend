bcfc2a731c59529d9edbb14ac3cbdb65f04f264c
Yuriy Li <0yuriyli@gmail.com>
2026-03-30 14:01:13 +0300

    fix:json serialization for docs

--- Models/Enums.cs
+++ Models/Enums.cs
@@ -1,11 +1,29 @@
 ﻿using System.ComponentModel;
+using System.Text.Json.Serialization;
 
 namespace TandemBackend.Models
 {
     [Description("Enumerator contains {en,ru}")]
+    [JsonConverter(typeof(JsonStringEnumConverter))]
     public enum Languages
     {
         en,
         ru,
+    }
+
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public enum TaskType
+    {
+        quiz,
+        codeCompletion,
+        codeEditor,
+    }
+
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public enum TaskDifficulty
+    {
+        easy,
+        medium,
+        hard,
     }
 }
--- Models/TaskStat.cs
+++ Models/TaskStat.cs
@@ -1,25 +1,20 @@
 namespace TandemBackend.Models
 {
-    public enum TaskType
-    {
-        quiz,
-        codeCompletion,
-        codeEditor,
-    }
-
-    public enum TaskDifficulty
-    {
-        easy,
-        medium,
-        hard,
-    }
-
     public class TaskStat
     {
         public required int Id { get; set; }
         public required string UserId { get; set; }
         public required int TaskId { get; set; }
         public required TaskType TaskType { get; set; }
+        public required TaskDifficulty TaskDifficulty { get; set; }
+    }
+
+    public class TaskStatPost
+    {
+        public required int TaskId { get; set; }
+
+        public required TaskType TaskType { get; set; }
+
         public required TaskDifficulty TaskDifficulty { get; set; }
     }
 }
