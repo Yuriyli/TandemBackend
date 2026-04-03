ebf1ebf60359228506769e0bc487d04e78a99700
Yuriy Li <0yuriyli@gmail.com>
2026-04-03 13:37:33 +0300

    refactor: change models and add migration

--- Data/ApplicationContext.cs
+++ Data/ApplicationContext.cs
@@ -6,7 +6,8 @@ namespace TandemBackend.Data
     public class ApplicationContext : DbContext
     {
         public DbSet<ExampleModel> ExampleModels => Set<ExampleModel>();
-        public DbSet<Topic> Topics => Set<Topic>();
+        public DbSet<Topic> EnTopics => Set<Topic>();
+        public DbSet<Topic> RuTopics => Set<Topic>();
         public DbSet<AppUser> Users => Set<AppUser>();
         public DbSet<TaskStat> TaskStats => Set<TaskStat>();
 
--- Models/Topic.cs
+++ Models/Topic.cs
@@ -1,43 +1,24 @@
-﻿using System.ComponentModel;
-
-namespace TandemBackend.Models
+﻿namespace TandemBackend.Models
 {
     public class Topic
     {
         public required int Id { get; set; }
         public required string Title { get; set; }
-        public required string Content { get; set; }
-        public required string TitleRu { get; set; }
-        public required string ContentRu { get; set; }
+        public required string Description { get; set; }
+        public required string Example { get; set; }
     }
 
     public class TopicPut
     {
-        [Description("Title on english")]
         public required string Title { get; set; }
-
-        [Description("Content on english")]
-        public required string Content { get; set; }
-
-        [Description("Title on russian")]
-        public required string TitleRu { get; set; }
-
-        [Description("Content on russian")]
-        public required string ContentRu { get; set; }
+        public required string Description { get; set; }
+        public required string Example { get; set; }
     }
 
-    [Description("Short version with one chosen language")]
-    public class TopicMono
+    public class TopicGet
     {
-        public required int Id { get; set; }
         public required string Title { get; set; }
-        public required string Content { get; set; }
-    }
-
-    [Description("Contains only id and title on chosen language")]
-    public class TopicTitle
-    {
-        public required int Id { get; set; }
-        public required string Title { get; set; }
+        public required string Description { get; set; }
+        public required string Example { get; set; }
     }
 }
