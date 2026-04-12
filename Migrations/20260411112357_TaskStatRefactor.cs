611d6e02258fe1bfb0607759e7d1fe4b3d72c4e1
Yuriy Li <70540931+Yuriyli@users.noreply.github.com>
2026-04-12 03:03:52 +0300

    Refactor: stat user task (#11)
    * fix: delete unused sescription and imports
    
    * refactor: change models and add migration
    
    * refactor: StatUserTask controller

---
+++ Migrations/20260411112357_TaskStatRefactor.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TandemBackend.Migrations
+{
+    /// <inheritdoc />
+    public partial class TaskStatRefactor : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "TaskId",
+                table: "TaskStats",
+                newName: "WrongAnswers");
+
+            migrationBuilder.RenameColumn(
+                name: "IsFinished",
+                table: "TaskStats",
+                newName: "EarnedPoints");
+
+            migrationBuilder.AddColumn<int>(
+                name: "CorrectAnswers",
+                table: "TaskStats",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Difficulty",
+                table: "TaskStats",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LessonName",
+                table: "TaskStats",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CorrectAnswers",
+                table: "TaskStats");
+
+            migrationBuilder.DropColumn(
+                name: "Difficulty",
+                table: "TaskStats");
+
+            migrationBuilder.DropColumn(
+                name: "LessonName",
+                table: "TaskStats");
+
+            migrationBuilder.RenameColumn(
+                name: "WrongAnswers",
+                table: "TaskStats",
+                newName: "TaskId");
+
+            migrationBuilder.RenameColumn(
+                name: "EarnedPoints",
+                table: "TaskStats",
+                newName: "IsFinished");
+        }
+    }
+}
